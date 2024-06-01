0f072464cbe388cd72ee9efb45b2ad3659fca35e
Bunyamin Kalkan <bnkalkan41@gmail.com>
2024-06-01 11:44:48 +0300

    feat: preparations are completed

---
+++ api/src/main/java/com/example/FlightTicketReservationSystem/FlightTicketReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.FlightTicketReservationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightTicketReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightTicketReservationSystemApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/example/FlightTicketReservationSystem/FlightTicketReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FlightTicketReservationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightTicketReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
